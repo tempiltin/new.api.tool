32b6e3a5ea9dfed038a9831d90c40beeaff27222
tempiltin <temurbekshukurov0707@gmail.com>
2023-02-01 01:11:44 +0500

    asd

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.send(
+    {
+      name:"salom"
+    }
+  )
+});
+
+module.exports = router;
